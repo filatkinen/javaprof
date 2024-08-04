73cb4b14cbc2b5ba8e9a46ceda817f7dad7dda7f
fenych <filatkinen@gmail.com>
2024-08-04 22:14:32 +0300

    hw04-gc done

---
+++ hw04-gc/src/main/java/ru/otus/filatkinen/calculator/Summator.java
@@ -0,0 +1,60 @@
+package ru.otus.filatkinen.calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings({"java:S125"})
+public class Summator {
+    private int sum = 0;
+    private int prevValue = 0;
+    private int prevPrevValue = 0;
+    private int sumLastThreeValues = 0;
+    private int someValue = 0;
+    private int someValueLast = 0;
+    private final List<Data> listValues = new ArrayList<>(100_000);
+
+    // !!! сигнатуру метода менять нельзя
+    public void calc(Data data) {
+        listValues.add(data);
+        if (listValues.size() % 100_000 == 0) {
+            listValues.clear();
+            someValueLast = 0;
+        }
+        sum += data.getValue();
+
+        sumLastThreeValues = data.getValue() + prevValue + prevPrevValue;
+
+        prevPrevValue = prevValue;
+        prevValue = data.getValue();
+
+        if (!listValues.isEmpty()) {
+            someValueLast += listValues.getLast().getValue();
+        }
+
+
+        for (int idx = 0; idx < 3; idx++) {
+            someValue += (sumLastThreeValues * sumLastThreeValues / (data.getValue() + 1) - sum);
+            someValue = Math.abs(someValue) + listValues.size() + someValueLast;
+        }
+    }
+
+    public Integer getSum() {
+        return sum;
+    }
+
+    public Integer getPrevValue() {
+        return prevValue;
+    }
+
+    public Integer getPrevPrevValue() {
+        return prevPrevValue;
+    }
+
+    public Integer getSumLastThreeValues() {
+        return sumLastThreeValues;
+    }
+
+    public Integer getSomeValue() {
+        return someValue;
+    }
+}
