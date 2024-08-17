61e1a2492d1f339de90667f122a0e9cfecac89fe
fenych <filatkinen@gmail.com>
2024-08-17 15:09:53 +0300

    hw05-fixed simplifying

--- hw05-bytecode/src/main/java/ru/otus/filatkinen/proxy/LogInvocationHandler.java
+++ hw05-bytecode/src/main/java/ru/otus/filatkinen/proxy/LogInvocationHandler.java
@@ -2,10 +2,7 @@ package ru.otus.filatkinen.proxy;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,45 +11,17 @@ public class LogInvocationHandler implements InvocationHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(LogInvocationHandler.class);
     private final Object target;
-    private final List<MethodArgs> methodArgs;
-
-    private static class MethodArgs {
-        String name;
-        Class<?>[] types;
-
-        public MethodArgs(String name, Class<?>[] types) {
-            this.name = name;
-            this.types = types;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            MethodArgs that = (MethodArgs) o;
-            return Objects.equals(name, that.name) && Objects.deepEquals(types, that.types);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(name, Arrays.hashCode(types));
-        }
-    }
+    private final Set<Method> methods;
 
     public LogInvocationHandler(Object target) {
         this.target = target;
-        methodArgs = new ArrayList<>();
-        Method[] ms = target.getClass().getDeclaredMethods();
-        for (Method m : ms) {
-            if (m.isAnnotationPresent(Log.class)) {
-                methodArgs.add(new MethodArgs(m.getName(), m.getParameterTypes()));
-            }
-        }
+        methods = new HashSet<>();
+        Collections.addAll(methods, target.getClass().getInterfaces()[0].getMethods());
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        if (methodArgs.contains(new MethodArgs(method.getName(), method.getParameterTypes()))) {
+        if (methods.contains(method)) {
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.append("executed method: ");
             stringBuilder.append(method.getName());
